3da5e045e336523f739c09d6eac21b927cf9378e
Namelos <namelos@outlook.com>
2017-10-25 23:22:22 +0800

    add filtering actor test with expect no message

--- src/test/scala/aia/testdriven/FilteringActorTest.scala
+++ src/test/scala/aia/testdriven/FilteringActorTest.scala
@@ -4,6 +4,8 @@ import akka.actor._
 import akka.testkit.TestKit
 import org.scalatest.{MustMatchers, WordSpecLike}
 
+import scala.concurrent.duration._
+
 class FilteringActorTest extends TestKit(ActorSystem())
   with WordSpecLike with MustMatchers with StopSystemAfterAll {
   "A Filtering Actor" must {
@@ -28,6 +30,29 @@ class FilteringActorTest extends TestKit(ActorSystem())
 
       eventIds mustBe List(1, 2, 3, 4, 5)
       expectMsg(Event(6))
+    }
+
+    "filter out particular message using expectNoMsg" in {
+      import FilteringActor._
+
+      val props = FilteringActor.props(testActor, 5)
+      val filter = system.actorOf(props, "filter-2")
+      filter ! Event(1)
+      filter ! Event(2)
+      expectMsg(Event(1))
+      expectMsg(Event(2))
+      filter ! Event(1)
+      expectNoMessage(1 second)
+      filter ! Event(3)
+      expectMsg(Event(3))
+      filter ! Event(1)
+      expectNoMessage(1 second)
+      filter ! Event(4)
+      filter ! Event(5)
+      filter ! Event(5)
+      expectMsg(Event(4))
+      expectMsg(Event(5))
+      expectNoMessage(1 second)
     }
   }
 }
